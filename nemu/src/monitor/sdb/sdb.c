b60c7b67f54ef76061802ccc4022d9d788bec9a0
tracer-ysyx <tracer@ysyx.org>
2025-03-11 18:55:47 +0800

    > compile NEMU ysyx_???????? 余伟杰 Linux DESKTOP-3O7NO06 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:55:47 up  9:35,  1 user,  load average: 0.12, 0.14, 1.20

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -43,13 +43,13 @@ static char* rl_gets() {
 }
 
 static int cmd_c(char *args) {
-  nemu_state.state = NEMU_QUIT;
   cpu_exec(-1);
   return 0;
 }
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
