cc57e56dd6a4cfd04b61c541f27a2a41f7dd4f58
tracer-ysyx <tracer@ysyx.org>
2025-03-12 10:08:23 +0800

    > compile NEMU ysyx_???????? 余伟杰 Linux DESKTOP-3O7NO06 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:08:23 up 12 min,  1 user,  load average: 0.12, 0.16, 0.09

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -15,7 +15,7 @@
 
 #include <isa.h>
 #include "local-include/reg.h"
-
+int i = 0;
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -24,8 +24,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  printf("%s -- 0x%x\n",regs[0],*regs[0]);
-  printf("%s -- 0x%x\n",regs[1],*regs[1]);
+  for(i=0;i<32;i++)
+  {
+  printf("%s -- 0x%x\n",regs[i],*regs[i]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
