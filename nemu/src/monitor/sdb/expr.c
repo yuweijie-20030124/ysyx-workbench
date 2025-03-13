8c69440f0c18149659d7179c5759d868bf329484
tracer-ysyx <tracer@ysyx.org>
2025-03-13 17:11:00 +0800

    > compile NEMU ysyx_???????? 余伟杰 Linux DESKTOP-3O7NO06 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:11:00 up  7:48,  1 user,  load average: 0.07, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -132,7 +132,7 @@ static bool make_token(char *e) {//将输入字符串分解为token数组
   int position = 0;
   int i;
   regmatch_t pmatch;
-  //memset(tokens, 0, sizeof(tokens)); //tokens清零,防止后续出现计算错误
+  memset(tokens, 0, sizeof(tokens)); //tokens清零,防止后续出现计算错误
   nr_token = 0;
 
   while (e[position] != '\0' && nr_token < MAX_TOKEN_NUM) {
