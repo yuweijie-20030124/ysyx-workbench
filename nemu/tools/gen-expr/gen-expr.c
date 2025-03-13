58f4ba53e49ed214f16bd2befe1a840e60f2560c
tracer-ysyx <tracer@ysyx.org>
2025-03-13 20:40:48 +0800

    > compile NEMU ysyx_???????? 余伟杰 Linux DESKTOP-3O7NO06 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:40:48 up 11:18,  1 user,  load average: 0.00, 0.02, 0.06

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -62,9 +62,54 @@ static void gen_num() {
   }// random space
 }
 
-static void gen_rand_expr() {
-  //buf[0] = '\0';
+static void gen_num() {
+  uint32_t randnum = rand() & (uint8_t)(-1);// % uint32
+  sprintf(buf + ptr, "%u", randnum);
+  ptr += strlen(buf + ptr);
+  if (rand() & 1) {
+    buf[ptr] = ' ';
+    ptr += 1;
+  }// random space
+}
 
+/*
+ *将表达式展开成高度为depth+1的完全二叉树，将有 2 ** depth 个叶子结点。
+ *每个叶子结点代表 数值 或 （数值）。
+ *因此，depth深度的表达式，最长为：
+ * 2 ** depth * len("( 10位%d数字uint32 ) op")
+ *=2 ** depth * 16
+ *因此，最大深度 MAX（depth）= log2(65536 / 16) = 12
+ */
+static void gen_rand_expr(bool noMoreBrackets, uint8_t depth) {
+  int choose3;
+  if (noMoreBrackets)// Avoid multiple Brackets (( ... ))
+  {
+    if (depth == 12)// Avoid buf overflow
+    {
+      choose3 = 0;
+    }
+    else
+    {
+      choose3 = rand() % 2;//0, 1
+    }
+  }
+  else
+  {
+    if (depth == 12)// Avoid buf overflow
+    {
+      choose3 = rand() % 2 == 0 ? 0 : 2;//0, 2
+    }
+    else
+    {
+      choose3 = rand() % 3;// 0, 1, 2
+    }
+  }
+  
+  switch (choose3) {
+    case 0: gen_num(); break;
+    case 1: gen_rand_expr(false, depth + 1); gen_rand_op(); gen_rand_expr(false, depth + 1); break;
+    default: gen('('); gen_rand_expr(true, depth); gen(')'); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
