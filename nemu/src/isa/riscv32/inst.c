df24b13def3c43bfcaf7c57a9beaf207244d7a28
tracer-ysyx <tracer@ysyx.org>
2025-05-24 13:29:37 +0800

    > compile NEMU ysyx_???????? 余伟杰 Linux DESKTOP-3O7NO06 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:29:37 up  2:59,  1 user,  load average: 0.00, 0.02, 0.05

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,8 +23,8 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_B, TYPE_R, 
+  TYPE_N, // none 
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
