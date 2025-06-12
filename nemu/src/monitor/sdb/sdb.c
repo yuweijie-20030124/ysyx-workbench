375de3558f3090c4bc29525b557da8db72734b6d
tracer-ysyx <tracer@ysyx.org>
2025-06-12 12:33:33 +0800

    > compile NEMU ysyx_??????? 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:33:33 up  3:42,  1 user,  load average: 0.02, 0.04, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -137,7 +137,7 @@ static int cmd_x(char *args){
       printf("0x%08x  " , addr + i * 4 );
       for(int j =0 ; j < 4 ; j++){
           printf("0x%02x " , data & 0xff);
-          //data = data >> 8 ;
+          data = data >> 8 ;
       }
       printf("\n");
   }
