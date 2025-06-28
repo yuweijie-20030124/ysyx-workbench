1be71ffe4f31d56789d5aa6eb675c1e88bc5722f
tracer-ysyx <tracer@ysyx.org>
2025-06-28 21:18:27 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:18:27 up 13:09,  1 user,  load average: 0.00, 0.04, 0.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -76,6 +76,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
 #endif
 }
+
 static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
