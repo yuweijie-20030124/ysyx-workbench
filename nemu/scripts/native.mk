79b727eebb4390d4aa401356f28f893e5145808e
tracer-ysyx <tracer@ysyx.org>
2025-06-28 21:22:16 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:22:16 up 13:12,  1 user,  load average: 0.10, 0.08, 0.08

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,6 +26,7 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
+override ARGS += -b
 
 # Command to execute NEMU
 IMG ?=
@@ -36,7 +37,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
-	ARGS = "-b"
+#	ARGS = "-b"
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
