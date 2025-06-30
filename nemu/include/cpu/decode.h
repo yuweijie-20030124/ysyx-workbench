a460fbd90c51d8409b62e9d2710f611303d983cf
tracer-ysyx <tracer@ysyx.org>
2025-06-30 20:07:03 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:07:03 up 11:22,  1 user,  load average: 0.24, 0.17, 0.10

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -23,7 +23,7 @@ typedef struct Decode {
   vaddr_t snpc; // static next pc
   vaddr_t dnpc; // dynamic next pc
   ISADecodeInfo isa;
-  IFDEF(CONFIG_ITRACE, char logbuf[12]);
+  IFDEF(CONFIG_ITRACE, char logbuf[100]);
 } Decode;
 
 // --- pattern matching mechanism ---
