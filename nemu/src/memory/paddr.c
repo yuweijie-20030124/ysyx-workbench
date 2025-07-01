77aa4e0cf5af5a05d7026f20bab21de66e48dad1
tracer-ysyx <tracer@ysyx.org>
2025-07-01 11:39:29 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:39:29 up  2:41,  1 user,  load average: 0.50, 0.23, 0.14

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -55,7 +55,7 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  //printf("");
+  printf("1");
   out_of_bound(addr);
   return 0;
 }
