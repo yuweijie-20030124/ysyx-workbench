d36dcc0843f7238a3bcbcbe7b8d62589e2743394
tracer-ysyx <tracer@ysyx.org>
2025-07-01 10:49:25 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:49:25 up  1:53,  1 user,  load average: 0.26, 0.24, 0.20

--- nemu/src/cpu/ringbuffer.c
+++ nemu/src/cpu/ringbuffer.c
@@ -34,10 +34,7 @@ void printBuffer(CircularBuffer *cb) {
     }
     int idx = (cb->head + IRINGBUF_SIZE - cb->count) % IRINGBUF_SIZE;
     for (int i = 0; i < cb->count; i++) {
-        printf("%d", cb->head);
-        printf("*****");
-        printf("%d", cb->count);
-        if(i == cb->head){
+        if(i == cb->count){
         printf("->%s\n", cb->buffer[(idx + i) % IRINGBUF_SIZE]);
         }
         else{
