97e77ce81fe7f66d8b70e7353041bbd2d42d2d58
tracer-ysyx <tracer@ysyx.org>
2025-07-03 21:05:50 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:05:50 up  4:02,  1 user,  load average: 0.10, 0.09, 0.09

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -66,4 +66,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_MTRACE, Log("write in address = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data));
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
+  Log("weiwei");
 }
