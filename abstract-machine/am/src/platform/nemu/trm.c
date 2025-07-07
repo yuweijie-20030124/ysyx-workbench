adcd7d73b55fea6bd8bda3c446ce4917ee2c1bee
tracer-ysyx <tracer@ysyx.org>
2025-07-07 09:50:21 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:50:21 up 52 min,  1 user,  load average: 0.24, 0.12, 0.10

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -4,21 +4,22 @@
 extern char _heap_start;
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, PMEM_END);
+Area heap = RANGE(&_heap_start, PMEM_END); //结构用于指示堆区的启始和末尾
+
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
-void putch(char ch) {
+void putch(char ch) { //输出一个字符
   outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
   nemu_trap(code);
-
+  //里面是一个嵌入汇编语句，宏会把一个识别结束的结束码移动到通用寄存器中
   // should not reach here
   while (1);
 }
 
 void _trm_init() {
-  int ret = main(mainargs);
+  int ret = main(mainargs); //用户程序
   halt(ret);
 }
