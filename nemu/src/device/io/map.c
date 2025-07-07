c3efdfbe21219e0ac6d248a8e3f019e5788c798a
tracer-ysyx <tracer@ysyx.org>
2025-07-07 16:08:45 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:08:45 up  7:16,  1 user,  load average: 0.29, 0.26, 0.20

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -68,5 +68,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
-   //IFDEF(CONFIG_DTRACE, Log("write device %s : address in = " FMT_PADDR ", len = %d\n", addr, len));
+  IFDEF(CONFIG_DTRACE, Log("write device %s : address in = " FMT_PADDR ", len = %d\n",  map->name , addr, len));
 }
