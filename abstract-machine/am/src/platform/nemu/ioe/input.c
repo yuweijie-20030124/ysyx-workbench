4f77a91887978ec571e94c52045918804e72f2ae
tracer-ysyx <tracer@ysyx.org>
2025-07-10 15:42:09 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:42:09 up  6:20,  1 user,  load average: 0.94, 0.54, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,7 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t k = inl(KBD_ADDR);
-  printf("Scancode: 0x%04x, %s\n", k, kbd->keydown ? "DOWN" : "UP");
+  //printf("Scancode: 0x%04x, %s\n", k, kbd->keydown ? "DOWN" : "UP");
   kbd->keydown = (k & KEYDOWN_MASK ? true : false);
   kbd->keycode = k & ~KEYDOWN_MASK;
 }
