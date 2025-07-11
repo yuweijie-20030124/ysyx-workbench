e1caa718439b34627527e4892292735959797f81
tracer-ysyx <tracer@ysyx.org>
2025-07-11 17:25:40 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:25:40 up  8:29,  1 user,  load average: 0.22, 0.22, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -35,9 +35,9 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
 
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-
+  //int len = ctl->buf.end - ctl->buf.start;
 }
