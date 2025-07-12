7dd7746f35d53ae16ca16520e3e1a38e28c2abb5
tracer-ysyx <tracer@ysyx.org>
2025-07-12 14:27:28 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:27:28 up  5:24,  1 user,  load average: 0.13, 0.23, 1.34

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -36,6 +36,7 @@ void sdl_audio_callback(void *userdata, uint8_t *stream, int len){
   //printf("%d\n",len);
   uint32_t used_cnt = audio_base[reg_count];
   len = len > used_cnt ? used_cnt : len;  //保证读取不超过可用数据量
+  printf("%u\n",used_cnt);
   uint32_t sbuf_size = audio_base[reg_sbuf_size];
   //如果剩余数据(sbuf_pos + len)超过缓冲区大小(sbuf_size)，需要分两次拷贝，需要指针回到开头再输入
   if( (sbuf_pos + len) > sbuf_size ){
@@ -56,7 +57,7 @@ int init_sound() {
   s.freq = audio_base[reg_freq];
   s.channels = audio_base[reg_channels];
   s.samples = audio_base[reg_samples];
-  printf("%d\n",s.samples);
+  //printf("%d\n",s.samples);
   s.callback = sdl_audio_callback;
   int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
   if (ret == 0) {
