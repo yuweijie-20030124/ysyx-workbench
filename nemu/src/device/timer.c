b963393ba332a79273c410bd7f42b8d0154dfa5b
tracer-ysyx <tracer@ysyx.org>
2025-07-12 14:58:57 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:58:57 up  5:54,  1 user,  load average: 0.32, 0.10, 0.25

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -23,7 +23,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
   if (!is_write && offset == 4) {
     uint64_t us = get_time();
-    rtc_port_base[0] = us & 0xffff;
+    rtc_port_base[0] = us & 0xff;
     rtc_port_base[1] = us >> 32;
   }
 }
