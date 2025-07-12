aae25c13cf83965ebb2511898cf6dd06aefa23b5
tracer-ysyx <tracer@ysyx.org>
2025-07-12 14:32:05 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:32:05 up  5:28,  1 user,  load average: 0.42, 0.29, 1.07

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -32,12 +32,12 @@ static uint8_t *sbuf = NULL;  //hi一个循环使用的音频缓冲区
 static uint32_t *audio_base = NULL;
 
 void sdl_audio_callback(void *userdata, uint8_t *stream, int len){
-  //SDL_memset(stream, 0, len);
+  SDL_memset(stream, 0, len);
   //printf("%d\n",len);
   uint32_t used_cnt = audio_base[reg_count];
   len = len > used_cnt ? used_cnt : len;  //保证读取不超过可用数据量
-  printf("%u\n",used_cnt);
   uint32_t sbuf_size = audio_base[reg_sbuf_size];
+  printf("%u\n",sbuf_size);
   //如果剩余数据(sbuf_pos + len)超过缓冲区大小(sbuf_size)，需要分两次拷贝，需要指针回到开头再输入
   if( (sbuf_pos + len) > sbuf_size ){
     SDL_MixAudio(stream, sbuf + sbuf_pos, sbuf_size - sbuf_pos , SDL_MIX_MAXVOLUME);
