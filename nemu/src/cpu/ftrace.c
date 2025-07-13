88fc27b15343b2e00124e0b840ac7bec77f4e822
tracer-ysyx <tracer@ysyx.org>
2025-07-13 19:27:32 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:27:32 up 10:24,  1 user,  load average: 0.12, 0.12, 0.09

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -4,8 +4,8 @@
 #include <elf.h>
 #include <assert.h>
 
-int elf_value;
-char elf_name;
+int *elf_value;
+char *elf_name;
 
 void parse_elf(const char *elf_file) {
     // 打开 ELF 文件
@@ -88,8 +88,8 @@ void parse_elf(const char *elf_file) {
         unsigned char type = ELF32_ST_TYPE(sym->st_info);
         if (type == STT_FUNC) {
             const char *name = strtab + sym->st_name;
-            //elf_value[i] = sym->st_value;
-           // elf_name[i] = name;
+            elf_value[i] = sym->st_value;
+            elf_name[i] = *name;
             printf("0x%x:   %s\n", sym->st_value, name);
         }
     }
