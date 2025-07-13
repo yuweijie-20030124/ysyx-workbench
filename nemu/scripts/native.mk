31ab4d2338840218167e38cfcc5c6fc62763c136
tracer-ysyx <tracer@ysyx.org>
2025-07-13 16:18:58 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:18:58 up  7:15,  1 user,  load average: 0.06, 0.05, 0.07

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,7 +24,6 @@ $(BINARY):: compile_git
 
 # Some convenient rules
 override ARGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt 
-override ARGS += -f $(IMAGE).elf 
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt   # --log=指定日志文件的路径
 override ARGS += $(ARGS_DIFF)
