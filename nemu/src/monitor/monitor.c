df8c8a9a1245d7c612733541bc6a8eddf3507238
tracer-ysyx <tracer@ysyx.org>
2025-07-13 16:42:07 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:42:07 up  7:38,  1 user,  load average: 0.20, 0.12, 0.10

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -118,8 +118,8 @@ void init_monitor(int argc, char *argv[]) {
   parse_args(argc, argv);
 
   /* parse elf file*/
-  printf("%s!!",elf_file);
-  //parse_elf(elf_file);
+  //printf("%s!!",elf_file);
+  parse_elf(elf_file);
 
   /* Set random seed. */
   init_rand();
