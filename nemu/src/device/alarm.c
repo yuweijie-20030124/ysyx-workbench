5694e9be44656d78a30511559e2a7eafa0ef0279
tracer-ysyx <tracer@ysyx.org>
2025-07-14 15:41:34 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:41:34 up 1 day,  6:38,  1 user,  load average: 0.83, 0.39, 0.19

--- nemu/src/device/alarm.c
+++ nemu/src/device/alarm.c
@@ -17,17 +17,19 @@
 #include <device/alarm.h>
 #include <sys/time.h>
 #include <signal.h>
-
-#define MAX_HANDLER 8
+//定时器系统，主要用于周期性地触发一系列处理函数
+#define MAX_HANDLER 8 
+//handler数组最多存储8哥定时处理函数
 
 static alarm_handler_t handler[MAX_HANDLER] = {};
-static int idx = 0;
+static int idx = 0; //ids记录当前已组测的处理函数数量
 
+//注册定时处理函数，将传入的处理函数h添加到handler数组中，检查不超过max即可。
 void add_alarm_handle(alarm_handler_t h) {
   assert(idx < MAX_HANDLER);
   handler[idx ++] = h;
 }
-
+//信号处理函数，当定时信号sigvtalrm除法时被调用，遍历所有已组测的处理函数并依次执行他们
 static void alarm_sig_handler(int signum) {
   int i;
   for (i = 0; i < idx; i ++) {
@@ -35,9 +37,12 @@ static void alarm_sig_handler(int signum) {
   }
 }
 
+/*设置信号处理：
+将alarm_sig_handler注册为SIGVTALRM信号的处理函数
+使用sigaction()系统调用完成注册*/
 void init_alarm() {
   struct sigaction s;
-  memset(&s, 0, sizeof(s));
+  memset(&s, 0, sizeof(s)); //清空s中的值
   s.sa_handler = alarm_sig_handler;
   int ret = sigaction(SIGVTALRM, &s, NULL);
   Assert(ret == 0, "Can not set signal handler");
