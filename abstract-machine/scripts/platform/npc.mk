dea5fc7d7196257297496835003b9e85a7f77555
tracer-ysyx <tracer@ysyx.org>
2025-07-19 20:08:05 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:08:05 up  3:52,  1 user,  load average: 0.15, 0.10, 0.06

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -25,7 +25,11 @@ image: image-dep
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
+sim: image
+	$(MAKE) -C $(NPC_HOME) sim IMG=$(IMAGE).bin
+
 run: insert-arg
 	echo "TODO: add command here to run simulation"
+    
 
 .PHONY: insert-arg
