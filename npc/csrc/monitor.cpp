484cf00e2c361bd02016baa311e76f9e733986e7
tracer-ysyx <tracer@ysyx.org>
2025-07-20 14:08:06 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:08:06 up 21:27,  1 user,  load average: 0.57, 3.02, 5.94

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -21,7 +21,8 @@ static void welcome() {//打印一些基本信息
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
-  printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
+  printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(RISCV32), ANSI_FG_YELLOW ANSI_BG_RED));
+  //printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
   Log("Exercise: Please remove me in the source code and compile NEMU again.");
   //assert(0);
--- npc/obj_dir/Vysyx_25060170_top.mk
+++ npc/obj_dir/Vysyx_25060170_top.mk
@@ -43,7 +43,6 @@ VM_USER_LDLIBS = \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
-	MEM_DIP \
 	main \
 	memory \
 	monitor \
@@ -64,8 +63,6 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
-MEM_DIP.o: csrc/MEM_DIP.cpp 
-	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
 main.o: csrc/main.cpp 
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
 memory.o: csrc/memory.cpp 
