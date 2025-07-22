3c68f4592a69ae9447eb1df20b1b24a983be22c3
tracer-ysyx <tracer@ysyx.org>
2025-07-22 10:47:58 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:47:58 up 2 days, 17:21,  1 user,  load average: 0.69, 0.32, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -20,10 +20,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     return;
   uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  //uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
   for (int i = y; i < y+h; i++) {
     for (int j = x; j < x+w; j++) {
-      fb[w*i+j] = pixels[w*(i-y)+(j-x)]; //缓冲区是一个像素块
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)]; //缓冲区是一个像素块
     }
   }
   if (ctl->sync) {
