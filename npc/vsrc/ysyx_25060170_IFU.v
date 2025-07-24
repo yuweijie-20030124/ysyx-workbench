047efd8e564a67d9ee74441cce2b5caffbd7b52d
ysyx_25060170-yuweijie <479634193@qq.com>
2025-07-24 20:09:28 +0800

    ifu 优化 FINISH

--- npc/vsrc/ysyx_25060170_IFU.v
+++ npc/vsrc/ysyx_25060170_IFU.v
@@ -10,14 +10,17 @@ module ysyx_25060170_IFU (
     input        ready_i,
     output       ready_o
 );
+
+    parameter RESET_PC = 32'h8000_0000;
+
     wire [31:0] pc_plus_4;
     wire [31:0] pc_plus_or_jump;
     
-    assign pc_plus_4 = pc + 4;
-    assign ready_o = ready_i;
+    assign pc_plus_4 = pc_o + 4;
+    assign ready_o = ready_i && !rst;
 
     // PC寄存器
-    Reg #(32, 32'h8000_0000) pc_reg (
+    Reg #(32, RESET_PC) pc_reg (
         .clk(clk),
         .rst(rst),
         .din(pc_plus_or_jump),
@@ -30,5 +33,10 @@ module ysyx_25060170_IFU (
         1'b0, pc_plus_4,
         1'b1, Jump_addr
     });
+
+    always @(posedge clk) begin
+        if (Jump_en && ready_i) 
+            $display("IFU: Jump to 0x%h at cycle %t", Jump_addr, $time);
+    end
 
 endmodule
