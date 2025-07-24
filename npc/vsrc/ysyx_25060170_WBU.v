2d40cdf11e07310dc7ab76aff1e9484715f675f5
ysyx_25060170-yuweijie <479634193@qq.com>
2025-07-24 19:46:07 +0800

    WBU FINISH

--- npc/vsrc/ysyx_25060170_WBU.v
+++ npc/vsrc/ysyx_25060170_WBU.v
@@ -9,6 +9,8 @@ ALUsrc-操作数选择信号，0-选择寄存器，1-选择立即数；
 RegW-寄存器堆写使能；
 PCx1-jalr处理信号，若为jalr则将x1+offset的值写入PC；
 */
+
+
 module ysyx_25060170_WBU(
     //from exu
     input [31:0] exu_result_i,   // EXU计算结果
@@ -20,12 +22,7 @@ module ysyx_25060170_WBU(
     input [31:0] pc_i,
 
     //from ControlUnit
-    input jal_flag,
-    input branch_flag,
-    input brlt_flag,
-    input regS_flag,
-    input regw_flag,
-    input PCx1,
+    input [1:0] regS,
 
 
     output [31:0] reg_write_data_o, // 写回寄存器的数据
@@ -34,7 +31,12 @@ module ysyx_25060170_WBU(
 );
 
     
-    assign reg_write_data_o = exu_result_i;
+    //assign reg_write_data_o = exu_result_i;
+    assign reg_write_data_o = (regS == 0) ? exu_result_i :  //0-来源于ALU
+                              (regS == 1) ? exu_result_i :  //1-来源于DataMem 感觉可以在exu和他搞成一样的
+                              (regS == 2) ? pc_i + 4 :      //2-来源于PC+4；
+                              32'b0;
+
     assign reg_write_addr_o = rd_i;
     assign reg_write_en_o   = (rd_i != 0); // x0不写
 
