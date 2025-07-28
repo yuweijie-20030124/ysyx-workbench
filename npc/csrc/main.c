1ae175bd381538df502055d4d9ee8aeb571267e3
tracer-ysyx <tracer@ysyx.org>
2025-07-28 16:06:07 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:06:07 up  1:35,  1 user,  load average: 0.37, 0.21, 0.19

--- npc/csrc/main.c
+++ npc/csrc/main.c
@@ -30,7 +30,8 @@ extern "C" void pmem_read(paddr_t raddr, paddr_t* rdata, int rlen){
   if (raddr < CONFIG_MEM_BASE) return;
   if (likely(in_pmem(raddr))) {
     *rdata = host_read(guest_to_host(raddr),rlen);
-    //printf("rdata = 08x%08x******\n",*rdata);
+    printf("C:raddr = 0x%08x\n",raddr);
+    printf("C:rdata = 0x%08x\n",*rdata);
     return;
     }
    //IFDEF(CONFIG_DEVICE, *rdata = mmio_read(raddr, rlen); /*printf("%lx\n",raddr);*/return);
