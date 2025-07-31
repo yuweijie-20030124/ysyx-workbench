14ce1aa4bde9506bed009d8df4c052cb17465c38
tracer-ysyx <tracer@ysyx.org>
2025-07-31 20:49:54 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:49:54 up 1 day, 11:11,  1 user,  load average: 0.06, 0.13, 0.24

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -40,6 +40,7 @@ static void out_of_bound(paddr_t addr) {
 
 //读物理地址
 word_t paddr_read(paddr_t addr, int len) {
+  //printf("进来了\n"); 
   if (likely(in_pmem(addr))) { 
     //printf("进来了\n"); 
     IFDEF(CONFIG_MTRACE, Log("read in address = " FMT_PADDR ", len = %d\n", addr, len));
