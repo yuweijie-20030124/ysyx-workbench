4cfe2629ece4fc05ff20a26c8ce38ecfd1e40961
tracer-ysyx <tracer@ysyx.org>
2025-07-31 11:00:33 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:00:33 up 1 day,  1:30,  1 user,  load average: 0.41, 0.41, 0.36

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -38,7 +38,7 @@ int update_watchpoint(void);
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); } //感觉在这里是输出指令的日志
+  if (ITRACE_COND) { log_write("%swocaosinidema\n", _this->logbuf); } //感觉在这里是输出指令的日志
 #endif
   //一次执行十条以下的指令gps就会赋值为true。
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
