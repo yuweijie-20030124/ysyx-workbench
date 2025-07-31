e23014c4f0316eefda0c6d7bcf390bcb693d3bbe
tracer-ysyx <tracer@ysyx.org>
2025-07-31 15:57:01 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:57:01 up 1 day,  6:24,  1 user,  load average: 0.19, 0.28, 0.57

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -65,6 +65,7 @@ void init_disasm() {
 // pc: 指令的地址
 // code: 指令的机器码
 // nbyte: 指令的字节数
+
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
 	cs_insn *insn;
 	size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
