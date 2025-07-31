f9e9008f9ef569aa3811d5b25b8d7084b3dd818c
tracer-ysyx <tracer@ysyx.org>
2025-07-31 15:42:19 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:42:19 up 1 day,  6:10,  1 user,  load average: 0.39, 0.28, 0.98

--- npc/csrc/cpu-exec.c
+++ npc/csrc/cpu-exec.c
@@ -43,7 +43,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   //printf("0x%08x\n",pc);
   s->snpc = get_pc();//静态下一条指令地址，默认为pc+4
   int inst_from_verilog = get_inst();
-  //printf("instformverilog is 0x%08x\n", inst_from_verilog);
+  printf("instformverilog is 0x%08x\n", inst_from_verilog);
   isa_exec_once();
 #ifdef CONFIG_ITRACE//如果启用了 CONFIG_ITRACE，会记录指令的详细信息到日志缓冲区 s->logbuf：
   char *p = s->logbuf;
