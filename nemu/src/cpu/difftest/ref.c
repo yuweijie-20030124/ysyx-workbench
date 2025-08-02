7e4de365848750f791a146fea4539f1b9d34d6b8
tracer-ysyx <tracer@ysyx.org>
2025-08-02 15:17:29 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:17:29 up  5:54,  1 user,  load average: 0.61, 0.30, 0.26

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -29,6 +29,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
     paddr_write(addr + i, 1, *((uint8_t*)buf + i));
       }
     }
+    else assert(0);
 }
 
 // __EXPORT void difftest_regcpy(void *dut, bool direction) {
