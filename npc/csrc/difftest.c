d48cf2046883c092259c2d8d2fa542f47f3a3b92
tracer-ysyx <tracer@ysyx.org>
2025-08-02 10:39:16 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:39:16 up  1:26,  1 user,  load average: 0.20, 0.35, 0.27

--- npc/csrc/difftest.c
+++ npc/csrc/difftest.c
@@ -16,10 +16,7 @@
 #include <dlfcn.h>
 #include <common.h>
 #include <isa.h>
-#include <cpu/cpu.h>
-#include <memory.h>
-#include <utils.h>
-#include <difftest-def.h>
+
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
