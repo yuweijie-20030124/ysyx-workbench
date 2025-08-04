b42c62b7e14c164f2af72c49c0251f72ae714573
tracer-ysyx <tracer@ysyx.org>
2025-08-04 13:57:30 +0800

    > run NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:57:30 up 23:42,  1 user,  load average: 1.18, 0.79, 0.58

--- npc/vsrc/ysyx_25060170_IDU.v
+++ npc/vsrc/ysyx_25060170_IDU.v
@@ -221,6 +221,7 @@ assign imm_o = imm;
                     ({32{auipc == 1'b1}} & {pc_i}) |
                     //lui   u-type
                     ({32{lui == 1'b1}} & {imm}) |
+                    
                     //addi  i-type
                     ({32{opcode == 7'b0010011}} & {reg1_rdata_i}) |
                     //add  i-type
