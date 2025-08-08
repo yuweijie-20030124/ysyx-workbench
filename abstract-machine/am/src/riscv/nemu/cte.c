d06c7b8b90d838e040e092903b927e1eb41d6701
tracer-ysyx <tracer@ysyx.org>
2025-08-08 14:58:48 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:58:48 up 1 day,  5:02,  1 user,  load average: 0.11, 0.11, 0.23

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,7 +19,9 @@ Context* __am_irq_handle(Context *c) {
 }
 
 extern void __am_asm_trap(void);
-
+//函数作用：接收参数，上面的软件需要给cte传handler，第一件事情设置mtvec，设置的入口在__am_asm_trap
+//以后处理器抛异常，pc就会跳转到mtvec的这个__am_asm_trap地址里面，在trap.S中定义了__am_asm_trap。
+//第二件事情把传进来的handler回调函数做一个注册，设置到一个全局变量中
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
