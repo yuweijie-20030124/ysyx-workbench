075b822fa6385edc457db7eef7dd40b67e75996b
tracer-ysyx <tracer@ysyx.org>
2025-08-09 15:54:25 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:54:25 up 2 days,  4:45,  1 user,  load average: 0.20, 0.35, 0.27

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -38,7 +38,7 @@ static debug_module_config_t difftest_dm_config = {
 
 struct diff_context_t {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
-  word_t mstvec;  //0x305
+  word_t mtvec;  //0x305
   word_t mstatus; //0x300
   word_t mepc;    //0x341
   word_t mcause;  //0x342
@@ -63,11 +63,11 @@ void sim_t::diff_get_regs(void* diff_context) {
   for (int i = 0; i < NR_GPR; i++) {
     ctx->gpr[i] = state->XPR[i];
   }
-  ctx -> mstvec = state -> mstvec;
-  ctx -> mstatus = state -> mstatus;
-  ctx -> mepc = state -> mepc;
-  ctx -> mcause = state -> mcause;
-  ctx -> pc = state->pc;
+  ctx -> mtvec    = state->mtvec;  // 修正拼写错误
+  ctx -> mstatus  = *(state->mstatus);  // 解引用 shared_ptr
+  ctx -> mepc     = *(state->mepc);
+  ctx -> mcause   = *(state->mcause);
+  ctx -> pc       = state->pc;
 }
 
 void sim_t::diff_set_regs(void* diff_context) {
