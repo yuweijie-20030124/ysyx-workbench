d61e0a3c1fb8e58a10f18f2e0668e9917307994e
tracer-ysyx <tracer@ysyx.org>
2025-08-09 16:29:21 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:29:21 up 2 days,  5:19,  1 user,  load average: 0.66, 0.88, 0.85

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -37,19 +37,19 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     return false;
   }
   if (ref_r->mstatus != cpu.mstatus) {
-    printf("csr reg mtvec is wrong\n");
+    printf("csr reg mstatus is wrong\n");
     printf("wrong reg value is 0x%08x\n",cpu.mstatus);
     printf("right reg value is 0x%08x\n",ref_r->mstatus);
     return false;
   }
   if (ref_r->mepc != cpu.mepc) {
-    printf("csr reg mtvec is wrong\n");
+    printf("csr reg mepc is wrong\n");
     printf("wrong reg value is 0x%08x\n",cpu.mepc);
     printf("right reg value is 0x%08x\n",ref_r->mepc);
     return false;
   }
   if (ref_r->mcause != cpu.mcause) {
-    printf("csr reg mtvec is wrong\n");
+    printf("csr reg mcause is wrong\n");
     printf("wrong reg value is 0x%08x\n",cpu.mcause);
     printf("right reg value is 0x%08x\n",ref_r->mcause);
     return false;
