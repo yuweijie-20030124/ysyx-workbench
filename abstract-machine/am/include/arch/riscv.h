74b73b07151ccc3411ad80f7218f71867af32e69
tracer-ysyx <tracer@ysyx.org>
2025-08-09 11:48:31 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:48:31 up 2 days, 40 min,  1 user,  load average: 0.17, 0.45, 0.39

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -9,7 +9,7 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
+  uintptr_t mcause, mstatus, mepc, gpr[NR_REGS];
   void *pdir;
 };
 
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,7 +19,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''. 待办事项：使用“NO”触发中断/异常。
    * Then return the address of the interrupt/exception vector. 然后返回中断/异常向量的地址
    */
-  cpu.mstatus = 0x00001800; //1000 0100 0111 1111 0101 0101 1111 1111
+  cpu.mstatus = 0x00001800; 
   cpu.mepc = epc; 
   cpu.mcause = NO;
   return cpu.mtvec;
