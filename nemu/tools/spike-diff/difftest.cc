535498d40e5c03fc20e299bd1212357f6cd6db53
tracer-ysyx <tracer@ysyx.org>
2025-08-09 16:18:08 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:18:08 up 2 days,  5:08,  1 user,  load average: 2.04, 1.65, 0.91

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -39,10 +39,13 @@ static debug_module_config_t difftest_dm_config = {
 struct diff_context_t {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   word_t pc;
-  word_t mtvec;
-  vaddr_t mepc;
-  word_t mstatus;
   word_t mcause;
+  word_t mstatus;
+  vaddr_t mepc;
+  word_t mtvec;
+  
+  
+
 };
 
 static sim_t* s = NULL;
