bb3024d79fe19b653f37fbf5060dcce47a3d501d
tracer-ysyx <tracer@ysyx.org>
2025-08-09 10:04:22 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:04:22 up 1 day, 23:03,  1 user,  load average: 0.41, 0.29, 0.21

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -98,7 +98,6 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
     isa_reg_display();
-    printf("caosinimendema\n");
   }
 }
 
