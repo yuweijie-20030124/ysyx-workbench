6cbc14e241a8fa1ecd7e796488f108676a0f2869
tracer-ysyx <tracer@ysyx.org>
2025-08-11 10:27:03 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:27:03 up 3 days, 21:34,  1 user,  load average: 0.07, 0.19, 0.21

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,8 +36,8 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 //此时可以访问上下文中的参数了。
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *cp = (Context *)(kstack.end - sizeof(Context));
-  cp->mepc = (unsigned long int)entry - 4;
-  cp->gpr[10] = (unsigned long int)arg;
+  cp->mepc = (uintptr_t)entry - 4;
+  cp->gpr[10] = (uintptr_t)arg;
   return cp;
 }
 
