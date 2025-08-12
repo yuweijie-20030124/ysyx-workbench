bf81b008fad3e15be0ed055c76634d0659accf22
tracer-ysyx <tracer@ysyx.org>
2025-08-12 14:32:38 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:32:38 up 5 days,  1:01,  1 user,  load average: 0.41, 0.29, 0.13

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -22,7 +22,7 @@ extern const char *regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int reg_num = (int)(sizeof(cpu.gpr) / sizeof(cpu.gpr[0]));
   for (int i = 0; i < reg_num; i++) {
-    if (ref_r->gpr[i] == cpu.gpr[i]) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
       printf("reg %s is wrong\n",regs[i]);
       printf("wrong reg value is 0x%08x\n",cpu.gpr[i]);
       printf("right reg value is 0x%08x\n",ref_r->gpr[i]);
