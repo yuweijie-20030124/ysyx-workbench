77e018ed318bf90de0a0884a9c6aeef1cdd6ded3
tracer-ysyx <tracer@ysyx.org>
2025-08-14 20:20:48 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:20:48 up 7 days,  4:30,  1 user,  load average: 0.47, 0.23, 0.14

--- nemu/include/device/map.h
+++ nemu/include/device/map.h
@@ -48,6 +48,7 @@ static inline int find_mapid_by_addr(IOMap *maps, int size, paddr_t addr) {
   int i;
   for (i = 0; i < size; i ++) {//遍历一遍要找的addr中的size空间
     if (map_inside(maps + i, addr)) {//确认看的是IO的映射区域
+      printf("jinlaile\n");
       difftest_skip_ref();
       return i;
     }
