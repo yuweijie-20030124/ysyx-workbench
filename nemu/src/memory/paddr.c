c3e4c1fa80379c8cd16f3b95e075b94b11b19f36
tracer-ysyx <tracer@ysyx.org>
2025-08-14 19:29:54 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:29:54 up 7 days,  3:41,  1 user,  load average: 0.11, 0.14, 0.12

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,10 +52,10 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 }
 
-// static void out_of_bound(paddr_t addr) {
-//   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
-//       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
-// }
+static void out_of_bound(paddr_t addr) {
+  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+      addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+}
 
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
@@ -76,10 +76,9 @@ word_t paddr_read(paddr_t addr, int len) {
     IFDEF(CONFIG_MTRACE, Log("read in address = " FMT_PADDR ", len = %d\n", addr, len));
     return pmem_read(addr, len);
   }
-  return mmio_read(addr, len);
-  // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  // printf("\n\n\n\n\n\n\n");
-  //out_of_bound(addr);
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  printf("\n\n\n\n\n\n\n");
+  out_of_bound(addr);
   return 0;
 }
 
@@ -89,9 +88,9 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data);
     IFDEF(CONFIG_MTRACE, Log("write in address = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data));
     return; }
-  mmio_write(addr, len, data); return;
-  // IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  // printf("\n\n\n\n\n\n\n");
-  // out_of_bound(addr);
+  
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  printf("\n\n\n\n\n\n\n");
+  out_of_bound(addr);
   //Log("weiwei");
 }
