13c423bf63b1a7e488dda80e43cf88f9773fd277
tracer-ysyx <tracer@ysyx.org>
2025-08-14 19:22:07 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:22:07 up 7 days,  3:33,  1 user,  load average: 0.21, 0.17, 0.12

--- npc/obj_dir/Vysyx_25060170_top___024root__DepSet_hcf322213__0__Slow.cpp
+++ npc/obj_dir/Vysyx_25060170_top___024root__DepSet_hcf322213__0__Slow.cpp
@@ -173,6 +173,9 @@ VL_ATTR_COLD void Vysyx_25060170_top___024root___stl_sequent__TOP__0(Vysyx_25060
         = vlSelfRef.ysyx_25060170_top__DOT__u_ysyx_25060170_GPR__DOT____Vcellout__reg_gen__BRA__31__KET____DOT__reg_x__dout;
     Vysyx_25060170_top___024root____Vdpiimwrap_ysyx_25060170_top__DOT__u_ysyx_25060170_MEM__DOT__pmem_read_TOP(vlSelfRef.ysyx_25060170_top__DOT__PCout, vlSelfRef.__Vtask_ysyx_25060170_top__DOT__u_ysyx_25060170_MEM__DOT__pmem_read__1__rdata, 4U);
     vlSelfRef.ysyx_25060170_top__DOT__MEM_inst_o = vlSelfRef.__Vtask_ysyx_25060170_top__DOT__u_ysyx_25060170_MEM__DOT__pmem_read__1__rdata;
+    VL_WRITEF_NX("verilog PC   = 0x%08x\nverilog inst = 0x%08x\n",0,
+                 32,vlSelfRef.ysyx_25060170_top__DOT__PCout,
+                 32,vlSelfRef.ysyx_25060170_top__DOT__MEM_inst_o);
     if ((0x100073U == vlSelfRef.ysyx_25060170_top__DOT__MEM_inst_o)) {
         Vysyx_25060170_top___024root____Vdpiimwrap_ysyx_25060170_top__DOT__u_ysyx_25060170_IDU__DOT__set_npc_exit_TOP(vlSelfRef.ysyx_25060170_top__DOT__PCout, 0U);
     }
