1a0dd4dee6ea6540d5113be3706d49767f31f1ea
tracer-ysyx <tracer@ysyx.org>
2025-09-29 16:18:28 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux DESKTOP-3O7NO06 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:18:28 up 1 day,  5:10,  1 user,  load average: 0.31, 0.19, 0.14

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -22,7 +22,9 @@ CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINAR
 
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
-#@echo "$(aosid)"
+	@echo "$(mainargs)"
+	@echo "$(MAINARGS_MAX_LEN)"
+	@echo "$(MAINARGS_PLACEHOLDER)"
 
 image: image-dep
 	@$(OBJDUMP) -S -d $(IMAGE).elf > $(IMAGE).txt
