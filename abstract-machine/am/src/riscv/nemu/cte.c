4a964dacd8e79d18a1f9401108ef9bd808956f00
ysyx_25060170-yuweijie <479634193@qq.com>
2025-10-10 22:25:24 +0800

    magic inst finish

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -33,6 +33,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));  //把amasmtrap的地址传给mtvec
 
+  // register event handler
   user_handler = handler;
 
   return true;
