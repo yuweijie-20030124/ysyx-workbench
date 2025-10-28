5a3166ed83d1bab05d7142a5852839d3b9677e6f
tracer-ysyx <tracer@ysyx.org>
2025-10-28 14:13:00 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:13:00 up 30 min,  1 user,  load average: 0.33, 1.41, 1.19

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -1,4 +1,4 @@
-/***************************************************************************************
+/**************************************************************************************
 * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
 *
 * NEMU is licensed under Mulan PSL v2.
@@ -17,8 +17,8 @@
 #include <device/map.h>
 
 //修改设置
-#define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 200))
-#define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 150))
+#define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 400))
+#define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
 
 static uint32_t screen_width() {
   return MUXDEF(CONFIG_TARGET_AM, io_read(AM_GPU_CONFIG).width, SCREEN_W);
