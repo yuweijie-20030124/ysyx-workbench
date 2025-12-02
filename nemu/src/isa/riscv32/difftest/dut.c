959f4a0a781d27894c0beace0bb000b14fe124ae
tracer-ysyx <tracer@ysyx.org>
2025-12-02 10:08:06 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:08:06 up 17:22,  2 users,  load average: 0.55, 0.65, 0.75

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -55,7 +55,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   //   return false;
   // }
 
-  if (ref_r->pc != cpu.pc) {
+  if (ref_r->pc != cpu.pc + 1) {
     printf("pc is wrong\n");
     printf("wrong pc is 0x%08x\n",cpu.pc);
     printf("right pc is 0x%08x\n",ref_r->pc);
