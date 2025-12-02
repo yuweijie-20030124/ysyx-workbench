0b14840296128420959bc119a4f4d9844259fa8e
tracer-ysyx <tracer@ysyx.org>
2025-12-02 10:05:17 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:05:17 up 17:19,  2 users,  load average: 1.04, 0.76, 0.80

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -127,7 +127,8 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-
+  printf("ref_r.pc = 0x%08x\n",ref_r.pc);
+  printf("pc   = 0x%08x\n",pc);
   checkregs(&ref_r, pc);
 }
 #else
