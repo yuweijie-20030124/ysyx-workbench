b33e5e9faa56c152f058b2c7b95fe63515076cfd
tracer-ysyx <tracer@ysyx.org>
2026-01-22 20:59:18 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:18 up  4:54,  1 user,  load average: 5.19, 4.06, 3.33

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -197,9 +197,9 @@ static int decode_exec(Decode *s) {
     // printf("imm =0x%08x\n",imm);
     // printf("dnpc =0x%08x\n",s->dnpc);  
     // }
-    printf("src1 =%d\n",src1);printf("src2 =%d\n",src2);
-    printf("pc =0x%08x\n",s->pc);printf("imm =0x%08x\n",imm);
-    printf("dnpc =0x%08x\n",s->dnpc);
+    // printf("src1 =%d\n",src1);printf("src2 =%d\n",src2);
+    // printf("pc =0x%08x\n",s->pc);printf("imm =0x%08x\n",imm);
+    // printf("dnpc =0x%08x\n",s->dnpc);
     if(src1 == src2) s->dnpc = s->pc + (int32_t)imm);
   INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, if(src1 != src2) s->dnpc = s->pc + imm);
   INSTPAT("??????? ????? ????? 100 ????? 11000 11", blt    , B, s->dnpc = ((int32_t)src1< (int32_t)src2) ? s->pc + imm : s->dnpc);  
