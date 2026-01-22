3f2dca5609df0fcd98164a5eb1d3ac45a3e1ff5f
tracer-ysyx <tracer@ysyx.org>
2026-01-22 19:38:09 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:38:09 up  3:33,  1 user,  load average: 2.27, 2.81, 2.35

--- npc/include/common.h
+++ npc/include/common.h
@@ -28,14 +28,14 @@
 //monitor那里的#ifndef CONFIG_ITRACEinit_disasm("riscv64-pc-linux-gnu");#endif
 
 
- #define CONFIG_TRACE 1
+//  #define CONFIG_TRACE 1
 
-#define CONFIG_TRACE_END 10000
-#define CONFIG_TRACE_START 0
-#define CONFIG_ITRACE 1
+// #define CONFIG_TRACE_END 10000
+// #define CONFIG_TRACE_START 0
+// #define CONFIG_ITRACE 1
 // #define CONFIG_MTRACE 1
 // #define CONFIG_DIFFTEST 1
-#define CONFIG_ITRACE_IRINGBUF 1
+// #define CONFIG_ITRACE_IRINGBUF 1
 
 
 // #define CONFIG_WATCHPOINT 1
@@ -59,7 +59,7 @@
 
 
 
-// #define CONFIG_DEVICE 1
+#define CONFIG_DEVICE 1
 
 #ifdef CONFIG_DEVICE
 #define CONFIG_HAS_SERIAL 1
