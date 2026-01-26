9bda85871aaa5697e6484c48432171b5e5cc206b
tracer-ysyx <tracer@ysyx.org>
2026-01-26 15:15:29 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:29 up  4:00,  2 users,  load average: 2.49, 2.87, 2.58

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -22,7 +22,7 @@
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
-  word_t mhart;      // 
+  // word_t mhart;      // 
   word_t mcause;     // 存放触发异常的原因           0x342
   word_t mstatus;    // 存放处理器的状态             0x300
   word_t mepc;       // 存放触发异常的PC             0x341
