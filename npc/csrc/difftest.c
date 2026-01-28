31bbc262d0a7abeb22dcfbec7314f9609c636140
yuweijie-25060170 <yuweijie@qq.com>
2026-01-28 15:33:47 +0800

    finish debug npc的csr的difftest问题,接下来继续写ecall问题

--- npc/csrc/difftest.c
+++ npc/csrc/difftest.c
@@ -160,16 +160,16 @@ bool isa_difftest_checkregs(NPC_reg *ref_r, vaddr_t pc) {
       printf("mstatus : 0x%08x %-11d  "  , ref_r->csr[1], ref_r->csr[1]);
       printf("mepc    : 0x%08x %-11d  "  , ref_r->csr[2], ref_r->csr[2]);
       printf("mtvec   : 0x%08x %-11d  \n", ref_r->csr[3], ref_r->csr[3]);
-      printf("mhartid : 0x%08x %-11d  "  , ref_r->csr[3], ref_r->csr[4]);
-      printf("mscratch: 0x%08x %-11d  "  , ref_r->csr[3], ref_r->csr[5]);
+      printf("mhartid : 0x%08x %-11d  "  , ref_r->csr[4], ref_r->csr[4]);
+      printf("mscratch: 0x%08x %-11d  "  , ref_r->csr[5], ref_r->csr[5]);
       
       printf("\nNPC REGS:\n");
-      printf("mcause  : 0x%08x %-11d  "  , cpu.csr[0], cpu.csr[0]);
-      printf("mstatus : 0x%08x %-11d  "  , cpu.csr[1], cpu.csr[1]);
-      printf("mepc    : 0x%08x %-11d  "  , cpu.csr[2], cpu.csr[2]);
-      printf("mtvec   : 0x%08x %-11d  \n", cpu.csr[3], cpu.csr[3]);
-      printf("mhartid : 0x%08x %-11d  "  , cpu.csr[3], cpu.csr[4]);
-      printf("mscratch: 0x%08x %-11d  "  , cpu.csr[3], cpu.csr[5]);
+      printf("mcause  : 0x%08x %-11d  "  , cpu.csr[0]   , cpu.csr[0])   ; 
+      printf("mstatus : 0x%08x %-11d  "  , cpu.csr[1]   , cpu.csr[1])   ; 
+      printf("mepc    : 0x%08x %-11d  "  , cpu.csr[2]   , cpu.csr[2])   ; 
+      printf("mtvec   : 0x%08x %-11d  \n", cpu.csr[3]   , cpu.csr[3])   ; 
+      printf("mhartid : 0x%08x %-11d  "  , cpu.csr[4]   , cpu.csr[4])   ; 
+      printf("mscratch: 0x%08x %-11d  "  , cpu.csr[5]   , cpu.csr[5])   ; 
       return false;
     }
   }
