07022b2e201b3b3d0de546b27047a7e18697f286
yuweijie-25060170 <yuweijie@qq.com>
2026-01-28 20:35:13 +0800

    debug ecall pc

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -23,12 +23,12 @@ typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
   // word_t mhart;      // 
-  word_t mcause;     // 存放触发异常的原因           0x342
-  word_t mstatus;    // 存放处理器的状态             0x300
-  word_t mepc;       // 存放触发异常的PC             0x341
-  word_t mtvec;      // 指向trap处理函数的入口地址    0x305
-  word_t mhartid;    // 存放hart编号                0xf12
-  word_t mscratch;   // 临时寄存器可读可写            0x340
+  word_t mcause     ;   // 存放触发异常的原因            0x342
+  word_t mstatus    ;   // 存放处理器的状态             0x300
+  word_t mepc       ;   // 存放触发异常的PC             0x341
+  word_t mtvec      ;   // 指向trap处理函数的入口地址    0x305
+  word_t mhartid    ;   // 存放hart编号                0xf12
+  word_t mscratch   ;   // 临时寄存器可读可写            0x340
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
