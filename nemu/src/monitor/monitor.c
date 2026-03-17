16fe251ccb2aa1b55dbfd24df84ed7128bb427d1
tracer-ysyx <tracer@ysyx.org>
2026-03-17 23:03:18 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:03:18 up 14:43,  1 user,  load average: 1.96, 2.10, 1.27

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -119,7 +119,7 @@ static int parse_args(int argc, char *argv[]) {
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:f:e:i:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:f:e:i:m:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
@@ -137,7 +137,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-d,--diff=REF_SO            run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT              run DiffTest with port PORT\n");
         printf("\t-i,--img=img.bin            用户程序存放在0x80000000\n");
-        printf("\t-i,--mromimg=mrom_img.bin   用户程序存放在0x80000000\n");
+        printf("\t-m,--mromimg=mrom_img.bin   用户程序存放在0x80000000\n");
         printf("\n");
         exit(0);
     }
