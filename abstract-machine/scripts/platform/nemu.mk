620a26ab5ad67cdf66e6aa6b63144c2640e1e6a0
tracer-ysyx <tracer@ysyx.org>
2026-03-17 20:00:03 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:00:03 up 11:40,  1 user,  load average: 0.74, 0.78, 0.66

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -14,6 +14,7 @@ LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NEMUFLAGS += -f $(IMAGE).elf
+NEMUFLAGS += -i $(IMAGE).bin
 # NEMUFLAGS += -b
 
 MAINARGS_MAX_LEN = 64
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -49,6 +49,7 @@ void sdb_set_batch_mode(); //批处理模式
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+// static char *mrom_img_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -84,25 +85,31 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"ftrace"   , required_argument, NULL, 'f'},
+    {"img"      , required_argument, NULL, 'i'},
+    // {"mromimg"  , required_argument, NULL, 'm'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:f:e:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:f:e:i:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'f': elf_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 1: img_file = optarg; return 0;
+      case 'i': img_file = optarg; break;
+      // case 'm': mrom_img_file = optarg; break;
+      // case 1  : img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-        printf("\t-b,--batch              run with batch mode\n");
-        printf("\t-l,--log=FILE           output log to FILE\n");
-        printf("\t-f,--ftrace=ELF_FILE    ftrace ELF to log\n");
-        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
-        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-b,--batch                  run with batch mode\n");
+        printf("\t-l,--log=FILE               output log to FILE\n");
+        printf("\t-f,--ftrace=ELF_FILE        ftrace ELF to log\n");
+        printf("\t-d,--diff=REF_SO            run DiffTest with reference REF_SO\n");
+        printf("\t-p,--port=PORT              run DiffTest with port PORT\n");
+        printf("\t-i,--img=img.bin            用户程序存放在0x80000000\n");
+        // printf("\t-i,--mromimg=mrom_img.bin   用户程序存放在0x80000000\n");
         printf("\n");
         exit(0);
     }
