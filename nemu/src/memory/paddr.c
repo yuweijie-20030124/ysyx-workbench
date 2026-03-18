0baeebd979a68248b8b66f623829d33875df198d
tracer-ysyx <tracer@ysyx.org>
2026-03-18 21:09:39 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:09:39 up 12:28,  1 user,  load average: 2.35, 2.08, 1.50

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -172,12 +172,13 @@ void mromaddr_write(paddr_t addr, int len, word_t data) {
 
 //写mrom地址
 void sramaddr_write(paddr_t addr, int len, word_t data) {
+    printf("addr = 0x%08x",addr);
+  printf("data = 0x%08x",data);
   if (likely(in_sram(addr))) { 
     sram_write(addr, len, data);
     IFDEF(CONFIG_MTRACE, Log("write in address = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data));
     return; }
   sram_out_of_bound(addr);
-  printf("addr = 0x%08x",addr);
-  printf("data = 0x%08x",data);
+
   //Log("weiwei");
 }
