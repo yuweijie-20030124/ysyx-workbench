8b9885a150b8d9164bf8f503d937e656da81b596
tracer-ysyx <tracer@ysyx.org>
2026-03-23 23:52:07 +0800

    > compile NEMU ysyx_25060170 余伟杰 Linux yuweijie 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:52:07 up  3:12,  1 user,  load average: 2.19, 1.64, 1.64

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -13,7 +13,8 @@ extern char _sram_start;
 
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, SRAM_END); //结构用于指示堆区的启始和末尾
+// Area heap = RANGE(&_heap_start, SRAM_END);
+Area heap = RANGE(&_heap_start, (&_heap_start) + 1024); //结构用于指示堆区的启始和末尾
 
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
